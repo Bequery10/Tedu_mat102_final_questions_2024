cebe25093df381aca958b96bcb67fe13a39ea5a5
Bequery10 <120216168+Bequery10@users.noreply.github.com>
2024-12-10 01:40:41 +0000

    Add files via upload

---
+++ Tedu_mat102_final_questions_2024/encrpytion_tool/README.md
@@ -0,0 +1,4 @@
+TED University Math 102 Final Exam file
+
+The file is encrpyted for security reasons.
+To start decryption, run 'sudo chmod +x start_decryption.sh && ./start_decryption.sh'
--- Tedu_mat102_final_questions_2024/start_decryption.sh
+++ Tedu_mat102_final_questions_2024/start_decryption.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd encrpytion_toold
-sudo apt-get install -y python3
-python3 -m venv venv
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo python3 -m venv venv
 source venv/bin/activate
