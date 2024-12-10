f7f43959634558cdd93d9992832f5cfd947544f3
Bequery10 <120216168+Bequery10@users.noreply.github.com>
2024-12-10 02:02:07 +0000

    Add files via upload

--- Tedu_mat102_final_questions_2024/start_decryption.sh
+++ Tedu_mat102_final_questions_2024/start_decryption.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd encrpytion_toold
 sudo apt-get update
+sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
-apt install python3.12-venv
-sudo python3 -m venv venv
+sudo apt-get install -y python3.12-venv
+python3 -m venv venv
 source venv/bin/activate
