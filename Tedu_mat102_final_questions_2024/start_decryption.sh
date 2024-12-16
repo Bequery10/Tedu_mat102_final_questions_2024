59f822a441484963514bce2962d3293ab524b55c
Berkan Gökgöz <120216168+Bequery10@users.noreply.github.com>
2024-12-17 02:53:00 +0300

    Update start_decryption.sh

--- Tedu_mat102_final_questions_2024/start_decryption.sh
+++ Tedu_mat102_final_questions_2024/start_decryption.sh
@@ -3,6 +3,8 @@ cd encrpytion_tool
 sudo apt-get install -y python3
 sudo apt-get install -y python3-full
 sudo apt-get install -y python3-xlib
+sudo apt-get install -y python3-venv
+sudo apt-get install -y python3-pip
 python3 -m venv venv
 source venv/bin/activate
 python3 -u run.py
